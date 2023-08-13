6afa9e888e4f366ac5de493ae4ce356c23d44c34
Dima Zemnitski <g.fredex@gmail.com>
2023-08-14 01:04:02 +0300

    feat: added solution for numbers up to 999

--- src/index.js
+++ src/index.js
@@ -1,23 +1,46 @@
 module.exports = function toReadable(number) {
     let strNumber = '';
-    let numDigits = 0;
+    let tempNum = number;
+    let numLength = String(tempNum).length;
     const smalNumArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     const tensNumArr = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-    if (number <= 19) {
-        for (i = 0; i <= number; i++) {
-            if (number === i) strNumber = smalNumArr[i];
+    if (numLength === 3) {
+        tempNum = Math.trunc(number / 100);
+        for (i = 0; i <= tempNum; i++) {
+            if (i === 0) strNumber += '';
+            else if (i === tempNum) { strNumber += smalNumArr[i] + ' hundred '; }
         }
-    } else if (number >= 20) {
-        if (String(number).length === 2) {
-            for (i = 0; i <= tensNumArr.length; i++) {
-                if (String(number)[0] == (i + 2)) strNumber += tensNumArr[i];
-            }
-            for (i = 0; i <= smalNumArr.length; i++) {
-                if ((String(number)[1] == i) && (i > 0)) strNumber += ' ' + smalNumArr[i];
-                else strNumber += '';
-            }
+        tempNum = number - tempNum * 100;
+    }
+
+    numLength = String(tempNum).length;
+    if ((numLength === 2) && ((number % 100) >= 20)) {
+        tempNum = Math.trunc((number % 100) / 10);
+        console.log(tempNum)
+        for (i = 0; i <= tempNum; i++) {
+            if (tempNum === i) { strNumber += tensNumArr[i - 2]; }
+            // else if (i === 0) { strNumber += ''; }
+        }
+        tempNum = number % 100;
+        // console.log(tempNum)
+        tempNum = tempNum - Math.trunc((number % 100) / 10) * 10;
+        console.log(tempNum)
+        for (i = 0; i <= tempNum; i++) {
+            if (i === 0) { strNumber += ''; }
+            else if (i === tempNum) { strNumber += ' ' + smalNumArr[i]; }
+        }
+    } else if ((number % 100) < 20) {
+        tempNum = number % 100;
+        // console.log(tempNum);
+        for (i = 0; i <= tempNum; i++) {
+            if (i === 0) { strNumber += ''; }
+            else if (i === tempNum) { strNumber += smalNumArr[i]; }
         }
     }
+    if ((numLength === 1) && (number === 0)) strNumber = smalNumArr[0];
+
+    if (strNumber[strNumber.length - 1] == ' ') { strNumber = strNumber.slice(0, -1); }
+
     return strNumber;
 }
