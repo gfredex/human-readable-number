7f53023629068b4837111c48d6d2a700b50659a5
Dima Zemnitski <g.fredex@gmail.com>
2023-08-13 22:32:27 +0300

    feat: added processing of numbers up to 19

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 module.exports = function toReadable(number) {
-    let strinNumber = '';
+    let strNumber = '';
     const smalNumArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     const tensNumArr = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-    return strinNumber;
+    if (number <= 19) {
+        for (i = 0; i <= number; i++) {
+            if (number === i) strNumber = smalNumArr[i];
+        }
+    }
+    return strNumber;
 }
