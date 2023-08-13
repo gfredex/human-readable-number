7935977100a7775835df356b630d262a6995753c
Dima Zemnitski <g.fredex@gmail.com>
2023-08-13 22:16:25 +0300

    init: added general structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let strinNumber = '';
+    const smalNumArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tensNumArr = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    return strinNumber;
 }
